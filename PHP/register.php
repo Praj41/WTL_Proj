3330239c6fb5ba593b81ecdb88a42b8ff4106abb
Praj41 <pcbhamare.41@gmail.com>
2022-01-26 23:29:49 +0530

    Init Commit

--- PHP/register.php
+++ PHP/register.php
@@ -1,7 +1,7 @@
 <?php
 
 // 1. database credentials
-
+/*
 $host = "localhost";
 $db_name = "MealBox";
 $username = "root";
@@ -12,3 +12,12 @@ $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
 
 
 // 3. prepare select query
+$query = "INSERT INTO user (name, email, username, password) VALUES (?, ?, ?, ?)";
+$stmt = $con->prepare($query);
+
+
+// 4. bind parameters
+$stmt->bindParam(1, $_POST['t1']);
+*/
+
+echo "Hello" . $_POST['name'];
