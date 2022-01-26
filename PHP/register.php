964a05a12071a7f9b250e70f90795c04aa290bc3
Praj41 <pcbhamare.41@gmail.com>
2022-01-26 22:52:56 +0530

    Init Commit

--- PHP/register.php
+++ PHP/register.php
@@ -1,1 +1,14 @@
 <?php
+
+// 1. database credentials
+
+$host = "localhost";
+$db_name = "MealBox";
+$username = "root";
+$password = "root";
+
+// 2. connect to database
+$con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+
+
+// 3. prepare select query
